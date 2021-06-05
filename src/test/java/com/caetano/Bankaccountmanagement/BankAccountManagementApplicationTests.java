257cc5efcc006432adaddc6c0947e7564a208ecf
Caetano Borges <caetanoalcantara@gmail.com>
2021-06-05 18:03:33 -0300

    Commit inicial - criando projeto pelo maven

---
+++ src/test/java/com/caetano/Bankaccountmanagement/BankAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caetano.Bankaccountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
