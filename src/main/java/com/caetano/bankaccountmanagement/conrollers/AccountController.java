3faf8dba8017a9f57cfc1a9df5c7905a50f1c428
Caetano Borges <caetanoalcantara@gmail.com>
2021-06-07 21:04:20 -0300

    Tratamento de exceções e respostas de erro http - Tratamento de todas as possibilidades pensadas por mim; - Troquei o objeto do Credit para TransferDTO para fazer mais sentido - faltou corrigir um problema no delete de contas que contém transações registradas devido a tabela de transações guardar referencia para a conta.

--- src/main/java/com/caetano/bankaccountmanagement/conrollers/AccountController.java
+++ src/main/java/com/caetano/bankaccountmanagement/conrollers/AccountController.java
@@ -1,5 +1,6 @@
 package com.caetano.bankaccountmanagement.conrollers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,66 +14,66 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.caetano.bankaccountmanagement.DTO.AccountDTO;
+import com.caetano.bankaccountmanagement.DTO.TransferDTO;
 import com.caetano.bankaccountmanagement.entities.Account;
 import com.caetano.bankaccountmanagement.services.AccountService;
 
 @RestController
-@RequestMapping(value="accounts")
+@RequestMapping(value = "accounts")
 public class AccountController {
-	
+
 	@Autowired
-	//private AccountRepository accountRepository;
+	// private AccountRepository accountRepository;
 	private AccountService accountService;
-	
+
 	public AccountController() {
 	}
-	
+
 	@GetMapping
-    public ResponseEntity<List<Account>> findAll() {
-        List<Account> list = accountService.findAll();
-        return ResponseEntity.ok().body(list);
-    }
-	
-	@GetMapping(value="/{id}") 
-	public ResponseEntity<Account> findById(@PathVariable Long id){
-		Account account = accountService.findById(id);
-		
-		return ResponseEntity.ok().body(account);		
-	}
-	
-	@PostMapping(value="/insert")
-	public ResponseEntity<Account> insert(@RequestBody AccountDTO entity){
-		Account account = accountService.insert(entity);		
-		return ResponseEntity.ok().body(account);		
+	public ResponseEntity<List<Account>> findAll() {
+		List<Account> list = accountService.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Account> findById(@PathVariable Long id) {
+		Account account = accountService.findById(id);
+		return ResponseEntity.ok().body(account);
+	}
+
+	@PostMapping(value = "/insert")
+	public ResponseEntity<Account> insert(@RequestBody AccountDTO entity) {
+		Account account = accountService.insert(entity);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(account.getIdentifier()).toUri();
+		return ResponseEntity.created(uri).body(account);
+	}
 
 	@PutMapping("/update/{id}")
 	public ResponseEntity<Account> update(@PathVariable("id") long id, @RequestBody Account entity) {
-		Account account = accountService.update(id, entity);		
-		return ResponseEntity.ok().body(account);		
-	}
-	
-	@GetMapping(value="/balance/{id}") 
-	public ResponseEntity<Double> getBalanceById(@PathVariable Long id){
-		Double balance = accountService.getBalance(id);		
-		return ResponseEntity.ok().body(balance);		
-	}
-	
-	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<HttpStatus> deleteAccount(@PathVariable("id") Long id){
-		accountService.delete(id);
-		return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		
+		Account account = accountService.update(id, entity);
+		return ResponseEntity.ok().body(account);
 	}
 
-	@PutMapping("/credit/{id}")
-	public ResponseEntity<Account> credit(@PathVariable("id") long id, @RequestBody Account entity) {
-		Account account = accountService.credit(id, entity);		
-		return ResponseEntity.ok().body(account);		
+	@GetMapping(value = "/balance/{id}")
+	public ResponseEntity<Double> getBalanceById(@PathVariable Long id) {
+		Double balance = accountService.getBalance(id);
+		return ResponseEntity.ok().body(balance);
 	}
-	
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<HttpStatus> deleteAccount(@PathVariable("id") Long id) {
+		accountService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PostMapping("/credit/{id}")
+	public ResponseEntity<Account> credit(@PathVariable("id") long id, @RequestBody TransferDTO entity) {
+		Account account = accountService.credit(id, entity);
+		return ResponseEntity.ok().body(account);
+	}
 
 }
--- src/main/java/com/caetano/bankaccountmanagement/services/AccountService.java
+++ src/main/java/com/caetano/bankaccountmanagement/services/AccountService.java
@@ -2,13 +2,19 @@ package com.caetano.bankaccountmanagement.services;
 
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.caetano.bankaccountmanagement.DTO.AccountDTO;
+import com.caetano.bankaccountmanagement.DTO.TransferDTO;
 import com.caetano.bankaccountmanagement.entities.Account;
 import com.caetano.bankaccountmanagement.repositories.AccountRepository;
+import com.caetano.bankaccountmanagement.services.exceptions.BusinessException;
+import com.caetano.bankaccountmanagement.services.exceptions.EntityAlreadyExistsException;
+import com.caetano.bankaccountmanagement.services.exceptions.MissingRequiredParametersException;
 
 @Service
 public class AccountService {
@@ -18,44 +24,62 @@ public class AccountService {
 
 	public AccountService() {
 	}
-	
-	public List<Account> findAll(){
+
+	public List<Account> findAll() {
 		return accountRepository.findAll();
 	}
 
 	public Account findById(Long id) {
-		return accountRepository.findById(id).get();
+		if (id == null) {
+			throw new MissingRequiredParametersException("Identifier required");
+		}
+		return accountRepository.findById(id).orElseThrow(
+				() -> new EntityNotFoundException("Id not found " + id));
 	}
 
 	public Double getBalance(Long id) {
-		Account account = accountRepository.findById(id).get();
+		Account account = findById(id);
 		return account.getBalance();
 	}
-	
+
 	public void delete(Long id) {
-		accountRepository.deleteById(id);
+		Account account = findById(id);
+		accountRepository.delete(account);
 	}
-	
+
 	public Account insert(AccountDTO entity) {
+		if (entity.getIdentifier() == null) {
+			throw new MissingRequiredParametersException("Identifier required");
+		}
+		if (accountRepository.existsById(entity.getIdentifier())) {
+			throw new EntityAlreadyExistsException("Identifier  " + entity.getIdentifier() + " already exists");
+		}
 		Account account = new Account(entity);
 		account.setBalance(0.0);
 		return accountRepository.save(account);
 	}
-	
+
 	public Account update(long id, @RequestBody Account entity) {
-		
-		Account account = accountRepository.findById(id).get();
-		 
-		account.setName(entity.getName() != null? entity.getName() : account.getName());
-		account.setDescription(entity.getDescription() != null? entity.getDescription() : account.getDescription());
-		account.setStatus(entity.getStatus() != null? entity.getStatus() : account.getStatus());
+
+		Account account = findById(id);
+
+		account.setName(entity.getName() != null ? entity.getName() : account.getName());
+		account.setDescription(entity.getDescription() != null ? entity.getDescription() : account.getDescription());
+		account.setStatus(entity.getStatus() != null ? entity.getStatus() : account.getStatus());
 		return accountRepository.save(account);
 	}
-	
-	public Account credit(long id, @RequestBody Account entity) {
+
+	public Account credit(long id, @RequestBody TransferDTO entity) {
+		Account account = findById(id);
 		
-		Account account = accountRepository.findById(id).get();		 
-		account.credit(entity.getBalance());
+		if (entity.getAmount() == null) {
+			throw new MissingRequiredParametersException("Amount required");
+		}
+
+		if (entity.getAmount() <= 0) {
+			throw new BusinessException("Amount must be a positive value");
+		}
+		account.credit(entity.getAmount());
 		return accountRepository.save(account);
 	}
 
